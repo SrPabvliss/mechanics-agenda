c76f6da4ac89c981e3d5efb1bb91ccfe68f3e49b
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-07-11 04:05:19 -0500

    feat: :sparkles: Add reviewWeekAdapter function to convert API data into schedule week events

---
+++ features/reviews/adapters/reviewAdapter.ts
@@ -0,0 +1,42 @@
+import { IEvent } from '@/shared/interfaces/IEvents'
+import { IScheduleWeek } from '@/shared/interfaces/ISchedule'
+
+import { scheduleWeek } from '../../../shared/constants/schedule-week'
+import { IReviewApi } from '../models/IApiReview'
+
+export const reviewWeekAdapter = (data: IReviewApi[]): IScheduleWeek[] => {
+  // Crear una copia del scheduleWeek para evitar mutaciones
+  const schedule = JSON.parse(JSON.stringify(scheduleWeek)) as IScheduleWeek[]
+
+  // Convertir los datos del API en eventos y agrupar por hora
+  data.forEach((review) => {
+    const event: IEvent = {
+      id: parseInt(review.id),
+      title: `${review.owner}'s ${review.car}`,
+      label: review.plate,
+      startTime: review.startTime,
+      endTime: review.endTime,
+      color: review.color,
+    }
+
+    const startHour = review.startTime.split(':')[0] + ':00'
+    const startMinutes = parseInt(review.startTime.split(':')[1])
+
+    const date = review.date
+
+    const scheduleHour = schedule.find((s) => s.hour === startHour)
+    if (scheduleHour) {
+      if (!scheduleHour.events[date]) {
+        scheduleHour.events[date] = { events1: [], events2: [] }
+      }
+
+      if (startMinutes < 30) {
+        scheduleHour.events[date].events1.push(event)
+      } else {
+        scheduleHour.events[date].events2.push(event)
+      }
+    }
+  })
+
+  return schedule
+}
