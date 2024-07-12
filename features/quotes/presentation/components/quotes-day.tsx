3d833087639dab1cf7a4652913dec17a24603666
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-07-12 00:00:34 -0500

    feat: :sparkles: Add QuotesDay, QuotesMonth, and QuotesWeek components

---
+++ features/quotes/presentation/components/quotes-day.tsx
@@ -0,0 +1,29 @@
+'use client'
+import CalendarDay from '@/shared/components/calendar-day/calendar-day'
+import { ISchedule } from '@/shared/interfaces/ISchedule'
+import { useState } from 'react'
+
+import { quotesDayAdapter } from '../../adapters/quotes-adapter'
+import { apiQuote } from '../../models/IApiQuote'
+
+interface QuotesDayProps {
+  date: string
+}
+
+const QuotesDay = ({ date }: QuotesDayProps) => {
+  const [schedule, setSchedule] = useState<ISchedule[]>([])
+  const onChange = async (day: string) => {
+    // Filtrar las revisiones que están en el día seleccionado
+    const filter = apiQuote.filter((quote) => quote.date === day)
+    setSchedule(quotesDayAdapter(filter))
+  }
+
+  const onClick = (id: number) => {
+    // eslint-disable-next-line no-console
+    console.log(id)
+    // Code here
+  }
+  return <CalendarDay onChange={onChange} onClick={onClick} schedule={schedule} date={date} />
+}
+
+export default QuotesDay
--- features/quotes/presentation/views/quotes-view.tsx
+++ features/quotes/presentation/views/quotes-view.tsx
@@ -1,20 +1,34 @@
 import { usePathname, useRouter } from 'next/navigation'
 
 import { ContentLayout } from '@/core/layout/content/content-layout'
+import { ViewButtons } from '@/features/reviews/presentation/components/views-buttons'
 import React from 'react'
 
 import { Button } from '@/components/ui/button'
 
+import useQuotesView from '../../hooks/use-quotes-view'
+import QuotesDay from '../components/quotes-day'
+import QuotesMonth from '../components/quotes-month'
+import QuotesWeek from '../components/quotes-week'
+
 export const QuotesView = () => {
   const router = useRouter()
-  const newPath = `${usePathname()}/new`
-  const editPath = `${usePathname()}/edit/1`
+  const pathname = usePathname()
+  const newPath = `${pathname}/new`
+
+  const { view, date } = useQuotesView()
 
   return (
     <ContentLayout title="Citas">
-      <div>QuotesView</div>
-      <Button onClick={() => router.push(newPath)}>Crear</Button>
-      <Button onClick={() => router.push(editPath)}>Editar</Button>
+      <div className="mb-2 lg:flex lg:justify-between ">
+        <ViewButtons view={view} />
+        <Button className="hidden w-fit lg:block" onClick={() => router.push(newPath)}>
+          Crear
+        </Button>
+      </div>
+      {view === 'day' && <QuotesDay date={date} />}
+      {view === 'week' && <QuotesWeek />}
+      {view === 'month' && <QuotesMonth />}
     </ContentLayout>
   )
 }
