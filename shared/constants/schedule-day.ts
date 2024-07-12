3af668ee4db3633ae9c50e0eceb8efd950ab7ad0
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-07-12 00:03:24 -0500

    feat: :sparkles: Add scheduleDay constant for managing daily schedule

---
+++ shared/constants/schedule-day.ts
@@ -0,0 +1,59 @@
+import { ISchedule } from '../interfaces/ISchedule'
+
+export const scheduleDay: ISchedule[] = [
+  {
+    hour: '08:00',
+    activities1: [],
+    activities2: [],
+  },
+  {
+    hour: '09:00',
+    activities1: [],
+    activities2: [],
+  },
+  {
+    hour: '10:00',
+    activities1: [],
+    activities2: [],
+  },
+  {
+    hour: '11:00',
+    activities1: [],
+    activities2: [],
+  },
+  {
+    hour: '12:00',
+    activities1: [],
+    activities2: [],
+  },
+  {
+    hour: '13:00',
+    activities1: [],
+    activities2: [],
+  },
+  {
+    hour: '14:00',
+    activities1: [],
+    activities2: [],
+  },
+  {
+    hour: '15:00',
+    activities1: [],
+    activities2: [],
+  },
+  {
+    hour: '16:00',
+    activities1: [],
+    activities2: [],
+  },
+  {
+    hour: '17:00',
+    activities1: [],
+    activities2: [],
+  },
+  {
+    hour: '18:00',
+    activities1: [],
+    activities2: [],
+  },
+]
