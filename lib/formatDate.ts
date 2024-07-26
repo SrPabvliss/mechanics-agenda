ba4630df1519b4be692c14b15ec5c368098f9919
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-07-26 16:45:09 -0500

    feat: :sparkles: Update formatDate and formatTime functions

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs'
+
 import 'dayjs/locale/es'
 
 dayjs.locale('es')
@@ -15,4 +16,11 @@ export const formatMonthYear = (date: string | Date): string => {
 
 export const formatDate = (date: string | Date): string => {
   return dayjs(date).format('YYYY-MM-DD')
+}
+
+export const formatDateTime = (date: string | Date, time: string): string => {
+  if (!date || !time) {
+    return ''
+  }
+  return formatDate(date) + ' ' + time + ':00-05'
 }
