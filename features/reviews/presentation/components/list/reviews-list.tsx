d2b9f8eca2f324fcb51256e9b7bd95ab3f98d846
SrPabvliss <pablomartinvillacres@gmail.com>
2024-08-04 16:12:54 -0500

    style: :children_crossing: Added scroll views for jobs and reviews

--- features/reviews/presentation/components/list/reviews-list.tsx
+++ features/reviews/presentation/components/list/reviews-list.tsx
@@ -2,8 +2,6 @@ import ReviewNoContent from '@/public/reviews-no-content.webp'
 import NoContent from '@/shared/components/no-content'
 import React from 'react'
 
-import { ScrollArea } from '@/components/ui/scroll-area'
-
 import useReviewsList from '../../../hooks/use-reviews-day-list'
 import { ReviewListItem } from './review-list-item'
 import { ReviewListSkeleton } from './review-list-skeleton'
@@ -25,16 +23,17 @@ const List: React.FC = () => {
         src={ReviewNoContent.src}
         title="No hay revisiones disponibles"
         subtitle="Parece que no se han agendado revisiones para este día."
+        className="h-[calc(93vh_-_206px)]"
       />
     )
   }
 
   return (
-    <ScrollArea>
+    <div className="scrollbar-hide flex h-[calc(93vh_-_206px)] flex-col overflow-auto">
       <div className="mt-2 flex flex-col gap-4">
         {filteredItems?.map((item) => <ReviewListItem review={item} key={item.id} />)}
       </div>
-    </ScrollArea>
+    </div>
   )
 }
 
