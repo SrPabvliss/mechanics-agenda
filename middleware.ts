83f33d2b878bfd9132e947cf1a3adcae9138c436
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-08-04 23:34:59 -0500

    feat: handle unauthorized error in response error handler

--- middleware.ts
+++ middleware.ts
@@ -5,16 +5,13 @@ import { jwtDecode } from 'jwt-decode'
 import { IDecodedToken } from './features/auth/models/IDecodedToken'
 import { isRoleAllowed } from './lib/menu-list'
 import { ACCESS_TOKEN_COOKIE_NAME } from './shared/api/api-routes'
-import { deleteCookie } from './shared/api/cookies-util'
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
   const token = req.cookies.get(ACCESS_TOKEN_COOKIE_NAME)?.value?.replaceAll('"', '')
 
-  const isValid = true
-
   if (pathname === '/login') {
-    if (token && isValid) {
+    if (token) {
       return NextResponse.redirect(new URL('/quotes', req.url))
     }
     return NextResponse.next()
@@ -24,22 +21,20 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL('/login', req.url))
   }
 
-  if (!isValid) {
-    const response = NextResponse.next()
-    await deleteCookie(ACCESS_TOKEN_COOKIE_NAME, response)
-    return NextResponse.redirect(new URL('/login', req.url))
-  }
-
   if (!token) {
     return NextResponse.redirect(new URL('/login', req.url))
   }
 
-  const { role }: IDecodedToken = jwtDecode(token)
-
-  const isAllowed = isRoleAllowed(pathname, role)
-
-  if (!isAllowed) {
-    return NextResponse.redirect(new URL('/403', req.url))
+  try {
+    const { role }: IDecodedToken = jwtDecode(token)
+    const isAllowed = isRoleAllowed(pathname, role)
+    if (!isAllowed) {
+      return NextResponse.redirect(new URL('/403', req.url))
+    }
+  } catch (error) {
+    const response = NextResponse.redirect(new URL('/login', req.url))
+    response.cookies.delete(ACCESS_TOKEN_COOKIE_NAME)
+    return response
   }
 
   return NextResponse.next()
